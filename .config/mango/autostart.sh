8f298942a2124f7d0bab3d7635b12d27e5dd83f1
DaBloat <kurtrusselvillamor1206@gmail.com>
2025-11-02 12:54:44 +0800

    added more wallpapers, configure more script

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -2,6 +2,6 @@
 #test the link
 set +e
 
-swaybg -i ~/.config/mango/wallpapers/shinra.png &
+swaybg -i ~/.config/mango/wallpapers/green_night.png &
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
