7b67fffa9e89df0f733b1c5ec86bc4d19a8e93b6
DaBloat <kurtrusselvillamor1206@gmail.com>
2025-11-25 16:04:19 +0800

    Added configurations folder - modularization of mangowc config

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -4,6 +4,6 @@ set +e
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
 /usr/lib/xdg-desktop-portal-wlr &
 
-swaybg -i ~/.config/mango/wallpapers/green_night.png &
+swaybg -i ~/.config/mango/wallpapers/purple-skull.png &
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
